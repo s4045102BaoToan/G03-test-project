eb6238c26a9d9966fe25a553e8ccdaa6a2cad43e
Tran Huu Viet Hung <s3975170@rmit.edu.vn>
2024-05-10 09:42:49 +0700

    result

--- Triangle.java
+++ Triangle.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 50;
+        int b = 60;
+        double Triangle = 0.5 * a * b;
     System.out.println("Ba");
-    System.out.println("apple");
+    System.out.println("Result:");
+    System.out.println(Triangle);
 
     }
 }
