bde1dcca48296f5a911d245c8fc3211d1903eec9
Tran Huu Viet Hung <s3975170@rmit.edu.vn>
2024-05-10 09:37:11 +0700

    change

--- Triangle.java
+++ Triangle.java
@@ -1,12 +1,7 @@
 public class Triangle {
     public static void main(String[] args) {
-<<<<<<< HEAD
     System.out.println("Ba");
     System.out.println("apple");
-=======
-    System.out.println("Baana");
-    
->>>>>>> ea92b7c6c8b76ef99a14cc1f246cb6f4ea7d9b9e
 
     }
 }
